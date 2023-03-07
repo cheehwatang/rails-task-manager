f7a87ccaae86d591247919b2973428290a3f00ed
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-07 13:15:25 +0800

    Adds edit and update function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,16 +12,31 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(task_new_params)
     @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_all_params)
 
     redirect_to task_path(@task)
   end
 
   private
 
-  def task_params
+  def task_new_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def task_all_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
