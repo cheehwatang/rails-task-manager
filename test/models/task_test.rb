56aa9febbcbd22d0ceaf9fcd4fb78b36f085ce39
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-15 17:43:40 +0800

    Adds validations and tests for task model

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'valid task' do
+    task = Task.new(title: 'Title', details: 'Details', completed: true)
+    assert task.valid?
+  end
+
+  test 'invalid task without title' do
+    task = Task.new(details: 'Details', completed: true)
+    assert_not task.valid?
+  end
+
+  test 'invalid task without details' do
+    task = Task.new(title: 'Title', completed: true)
+    assert_not task.valid?
+  end
+
+  test 'valid task without completed' do
+    task = Task.new(title: 'Title', details: 'Details')
+    assert task.valid?
+  end
+
+  test 'task completed with false as default value' do
+    task = Task.new
+    assert_not task.completed
+  end
+
 end
